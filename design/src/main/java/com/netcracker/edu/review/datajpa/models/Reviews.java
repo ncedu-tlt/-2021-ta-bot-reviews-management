a2f3d14b1ececf6ad885ce78130f975bfd9a63e7
Alexander Kotsarev <cotsarev.alex@yandex.ru>
2022-02-20 11:38:40 +0400

    Jpa

---
+++ design/src/main/java/com/netcracker/edu/review/datajpa/models/Reviews.java
@@ -0,0 +1,40 @@
+package com.netcracker.edu.review.datajpa.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Reviews {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String review;
+
+
+    protected Reviews() {}
+
+    public Reviews(int  id, String review) {
+        this.id = id;
+        this.review = review;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Reviews[id=%d, review='%s']",
+                id, review);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+
+}
