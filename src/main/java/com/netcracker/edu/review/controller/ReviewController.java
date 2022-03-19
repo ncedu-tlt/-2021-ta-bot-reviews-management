935c7a93c8be6eb60c4a6a5f9238c9b86a6fddbc
Alexander Kotsarev <cotsarev.alex@yandex.ru>
2022-03-19 12:46:29 +0400

    UC19.1

--- src/main/java/com/netcracker/edu/review/controller/ReviewController.java
+++ src/main/java/com/netcracker/edu/review/controller/ReviewController.java
@@ -2,14 +2,13 @@ package com.netcracker.edu.review.controller;
 
 import com.netcracker.edu.review.model.Mark;
 import com.netcracker.edu.review.model.Review;
-
 import com.netcracker.edu.review.model.ui.UiReview;
 import com.netcracker.edu.review.service.MarkService;
 import com.netcracker.edu.review.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -23,8 +22,7 @@ public class ReviewController {
     public MarkService markService;
 
     @PostMapping("/add")
-    public ResponseEntity<Review> createReview(@RequestBody UiReview uiReview) {
-        Mark mark = markService.findMarkByValue(uiReview.getMark());
+    public ResponseEntity<Review> createReview(@RequestBody UiReview uiReview, Mark mark) {
         return ResponseEntity.ok(reviewService.createReview(uiReview, mark));
     }
 
@@ -32,4 +30,17 @@ public class ReviewController {
     public ResponseEntity<List<Review>> findReviewByAuthorId(@PathVariable("authorId") int authorId) {
         return ResponseEntity.ok(reviewService.findReviewByAuthorId(authorId));
     }
+
+    @PutMapping("/change/{id}")
+    public ResponseEntity<Review> updateReviewById(@PathVariable("id") int id, @RequestBody UiReview uiReviewUpdated, Mark mark) {
+        return ResponseEntity.ok(reviewService.updateReviewById(id, uiReviewUpdated, mark));
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<HttpStatus> deleteReviewById(@PathVariable("id") int id) {
+        reviewService.deleteReviewByid(id);
+        return ResponseEntity.accepted().build();
+    }
+
 }
--- src/main/java/com/netcracker/edu/review/repository/MarkRepository.java
+++ src/main/java/com/netcracker/edu/review/repository/MarkRepository.java
@@ -4,8 +4,6 @@ import com.netcracker.edu.review.model.Mark;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface MarkRepository extends JpaRepository<Mark,String> {
     Mark findMarkByValue(String value);
